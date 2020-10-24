efe68ecd3f6941c311f459ac4d680d176249d0b1
Stanislav <stanislav.ussov@gmail.com>
2020-10-24 17:33:59 +0200

    Socaial Network

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://www.udemy.com/staticx/udemy/images/v6/logo-coral-light.svg" />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+const Profile = () => {
+    return (
+        <div className="profile">
+            <img src="https://gray-wluc-prod.cdn.arcpublishing.com/resizer/L--Nt387d662hNpN2_GQnGRQ-O0=/1200x675/smart/cloudfront-us-east-1.images.arcpublishing.com/gray/CWX37DMGPZGUHHQOR5NB7LC5PM.png" />
+            <div>
+                Ava + desc
+            </div>
+            <div>
+                My Posts
+            </div>
+            <div>
+                New Posts
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
